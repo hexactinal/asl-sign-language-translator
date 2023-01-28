a7fc1975291cee391d3c597ccd1bedf6976cabc9
Jayson Van Dam <jaysonvandam@gmail.com>
2023-01-28 18:06:23 -0500

    added text to gui window

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ class_names = open("labels.txt", "r").readlines()
 
 # CAMERA can be 0 or 1 based on default camera of your computer
 camera = cv2.VideoCapture(1)
+output_text = "Placeholder"
 
 while True:
     # Grab the webcamera's image.
@@ -21,6 +22,9 @@ while True:
     # Resize the raw image into (224-height,224-width) pixels
     image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
 
+    # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    new_text = cv2.putText(image, output_text, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
+    # cv2.imwrite(image, new_text)
     # Show the image in a window
     cv2.imshow("Webcam Image", image)
 
@@ -37,8 +41,9 @@ while True:
     confidence_score = prediction[0][index]
 
     # Print prediction and confidence score
-    print("Class:", class_name[2:], end="")
-    print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
+    # print("Class:", class_name[2:], end="")
+    # print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
+    output_text = class_name[2:]
 
     # Listen to the keyboard for presses.
     keyboard_input = cv2.waitKey(1)
