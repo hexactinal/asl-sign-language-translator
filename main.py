57e13c5b1699c169e75462eb601531c1db608678
the-rks <roshanksadhu@gmail.com>
2023-01-29 10:01:29 -0500

    broke webcam :(

--- main.py
+++ main.py
@@ -10,6 +10,20 @@ QUIT_BUTTONS = set([
     27, # ASCII Esc
     113 # ASCII 'q'
 ])
+# Path for exported data, numpy arrays
+DATA_PATH = os.path.join("MP_Data")
+
+# Actions that we try to detect
+actions = np.array(["hello", "thanks", "iloveyou"])
+
+# Thirty videos worth of data
+no_sequences = 30
+
+# Videos are going to be 30 frames in length
+sequence_length = 30
+
+# Folder start
+start_folder = 30
 
 def mediapipe_detection(image, model):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB
@@ -47,29 +61,44 @@ def draw_styled_landmarks(image, results):
                              mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
                              )
 
+def extract_keypoints(results):
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
+    face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468*3)
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+    return np.concatenate([pose, face, lh, rh])
+
 mp_holistic = mp.solutions.holistic # holistic model
 mp_drawing = mp.solutions.drawing_utils # Drawing utilities
 
 cap = cv2.VideoCapture(0)
 # Set mediapipe model 
 with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-    while cap.isOpened():
+    
+    # NEW LOOP
+    # Loop through actions
+    for action in actions:
+        # Loop through sequences aka videos
+        for sequence in range(no_sequences):
+            # Loop through video length aka sequence length
+            for frame_num in range(sequence_length):
+                # Read feed
+                ret, frame = cap.read()
 
-        # Read feed
-        ret, frame = cap.read()
+                # Make detections
+                image, results = mediapipe_detection(frame, holistic)
+                # print(results)
 
-        # Make detections
-        image, results = mediapipe_detection(frame, holistic)
-        print(results)
-        
-        # Draw landmarks
-        draw_styled_landmarks(image, results)
-
-        # Show to screen
-        cv2.imshow('OpenCV Feed', image)
+                # Draw landmarks
+                draw_styled_landmarks(image, results)
+                
+                # Show to screen
+                cv2.imshow('OpenCV Feed', image)
 
         # Break gracefully ... press Esc or 'q' to quit.
-        if cv2.waitKey(1) in QUIT_BUTTONS:
+        # if cv2.waitKey(1) in QUIT_BUTTONS:
+        #     break
+        if cv2.waitKey(10) & 0xFF == ord('q'):
             break
 
     cap.release()
@@ -78,3 +107,21 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
 draw_landmarks(frame, results)
 plt.imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
 plt.show()
+pose = []
+# for res in results.pose_landmarks.landmark:
+#     test = np.array([res.x, res.y, res.z, res.visibility])
+#     pose.append(test)
+# pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(132)
+# face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(1404)
+# lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+# rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+
+# result_test = extract_keypoints(results)
+
+# for action in actions: 
+#     dirmax = np.max(np.array(os.listdir(os.path.join(DATA_PATH, action))).astype(int))
+#     for sequence in range(1,no_sequences+1):
+#         try: 
+#             os.makedirs(os.path.join(DATA_PATH, action, str(dirmax+sequence)))
+#         except Exception:
+#             pass
